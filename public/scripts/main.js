e5bf3dec762454499979e57e4be517a5830d064e
Maiki <maiki.moseley@gmail.com>
2021-02-03 14:15:18 -0600

    add more advanced styling and animations

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -17,6 +17,7 @@ $("#registration-form").submit(function(event){
       }
     }) 
   })
+
 /* LOGIN AJAX ROUTE */
 $("#login-form").submit(function(event){
     event.preventDefault();
@@ -38,21 +39,45 @@ $("#login-form").submit(function(event){
   })
 
 /* STATES */
+let state = "login";
 const reset = function(){
+  if(state==="login"){
+    $("#login").css("transform","translateX(-100%)");
+    $("#login").css("visibility", "hidden");
+  }
+  if(state==="register"){
+    $("#register").css("transform","translateY(100%)");
+    $("#register").css("visibility", "hidden");
+  }
+  if(state==="home"){
+    $("#nav-buttons").css("transform","translateX(-200%)");
+    $("#nav-buttons").css("visibility", "hidden");
+    $("#slide-bar").css("transform","skew(-40deg, 0deg) translateX(-150%)");
+    $("#slide-bar").css("visibility", "hidden");
+  }
   $(".hidable").css("visibility","hidden");
 }
 const login = function(){
   reset();
   $("#login").css("visibility","visible");
+  $("#login").css("transform","translateX(0%)")  
+  state="login";
 }
 
 const register = function(){
   reset();
   $("#register").css("visibility","visible");
+  $("#register").css("transform","translateY(0%)");
+  state="register";
 }
 
 const home = function(){
-  reset();  
+  reset(); 
+  $("#nav-buttons").css("visibility", "visible");
+  $("#nav-buttons").css("transform","translateX(0%)");
+  $("#slide-bar").css("visibility", "visible");
+  $("#slide-bar").css("transform","skew(-40deg, 0deg) translateX(0%)");
+  state="home"; 
 }
 
 /* LOGGED IN STATE TRIGGER */
