3079b8b508863b9bc1b887f93cf0fbfe69107fec
Maiki <maiki.moseley@gmail.com>
2021-02-07 14:34:10 -0600

    fix bug involving game variable

--- controllers/game.js
+++ controllers/game.js
@@ -5,27 +5,27 @@ const db = require("../models");
 /* Game Story Route */
 router.get("/:id/story", async function(req,res){
     const foundGame = await db.Game.findById(req.params.id);
-    res.render("main",{sentState: "story", game: foundGame})
+    res.render("main",{sentState: "story"})
 })
 /* Game Character Route */
 router.get("/:id/characters", async function(req,res){
     const foundGame = await db.Game.findById(req.params.id);
-    res.render("main",{sentState: "characters", game: foundGame})
+    res.render("main",{sentState: "characters"})
 })
 /* Game Player Route */
 router.get("/:id/players", async function(req,res){
     const foundGame = await db.Game.findById(req.params.id);
-    res.render("main",{sentState: "players", game: foundGame})
+    res.render("main",{sentState: "players"})
 })
 /* Game File Route */
 router.get("/:id/files", async function(req,res){
     const foundGame = await db.Game.findById(req.params.id);
-    res.render("main",{sentState: "files", game: foundGame})
+    res.render("main",{sentState: "files"})
 })
 
 /* Game Component: Story */
 router.get("/:id/story/story", async function(req, res){
-    res.render("components/game/story");
+    res.render("components/game/story",{game: foundGame});
 })
 /* Game Component: Characters */
 router.get("/:id/characters/characters", async function(req, res){
