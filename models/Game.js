a61e92f7936325111554e24e5dbb872c2af112c8
Maiki <maiki.moseley@gmail.com>
2021-05-07 17:04:26 -0500

    add start script

--- models/Game.js
+++ models/Game.js
@@ -5,6 +5,7 @@ const gameSchema = new mongoose.Schema(
     name: {type:String, required:true},
     gamemasters: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
     players: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    characters: [{name: {type:String, unique:true}, url: {type:String}}],
     chapters: [{type: mongoose.Schema.Types.ObjectId, ref: "Chapter"}],
     songs: [{name: {type:String, unique:true}, url: {type:String}}],
     images: [{name: {type:String, unique:true}, url: {type:String}}],
